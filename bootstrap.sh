3df0deaba3187ff6470c49b70f3f201919c6f1e2
Filip Krynicki <filipkrynicki@gmail.com>
2013-05-23 14:50:04 -0400

    made ipython install manually

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y python-pip python-dev build-essential sqlite3 libfreetype6-dev libpng-dev
-pip install numpy turkflow pyzmq tornado ipython
-pip install matplotlib
+apt-get install -y python-pip python-dev build-essential sqlite3 libfreetype6-dev libpng-dev python-scipy python-matplotlib
+pip install --upgrade ipython
+pip install turkflow
 
 ln -s -f /home/host/.boto /home/vagrant/.boto
 
