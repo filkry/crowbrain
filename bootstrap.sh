f7ba30b1429a8130627148a71e413acfc357df86
Filip Krynicki <filipkrynicki@gmail.com>
2013-05-23 13:52:58 -0400

    analysis of batch sampling

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ pip install matplotlib
 ln -s -f /home/host/.boto /home/vagrant/.boto
 
 mkdir -p /home/vagrant/.ipython
+chown vagrant /home/vagrant/.ipython
 
 if [[ ! -e /home/vagrant/.ipython/profile_server ]]; then
 	ln -s -f /vagrant/ipython_profile_server /home/vagrant/.ipython/profile_server
