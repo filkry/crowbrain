68c1da75169a0d767d404e56d3ceeb7922676021
Fil Krynicki <filipkrynicki@gmail.com>
2014-05-26 11:32:20 -0400

    testing pareto and zipf distributions for idea ranks

--- exps/p_idea_structure.py
+++ exps/p_idea_structure.py
@@ -4,11 +4,46 @@ import re, pystan, format_data, modeling, os
 import matplotlib.pyplot as plt
 import stats_fns as mystats
 from collections import defaultdict, OrderedDict
+from scipy.stats import pareto, zipf
 
 model_string = """
+data {
+    int <lower=0> N;
+    real <lower=1> rrank[N];
+}
+
+parameters {
+    real <lower=0> alpha;
+}
+
+model {
+    alpha ~ exponential(0.5); // prior on alpha
+
+    rrank ~ pareto(1, alpha);
 }
 """
 
+def gen_dat(freqs):
+    rank = []
+    for i, f in enumerate(freqs):
+        rank.extend([i+1] * f)
+
+    return {'rrank': rank,
+            'N': len(rank)}
+
+def plot_dist(ax, max_x, dist_fn):
+    xs = range(1, max_x)
+    ys = [dist_fn(x) for x in xs]
+    ax.plot(xs, ys, color='k')
+
+def plot_freqs_against_dist(ax, freqs, dist_fn):
+    total_freq = sum(freqs)
+    norm_freqs = [f / total_freq for f in freqs]
+
+    max_x = len(norm_freqs) + 1
+    ax.bar(range(1, max_x), norm_freqs, color='r', alpha=0.25)
+    plot_dist(ax, max_x, dist_fn)
+    
 def plot_geometric_series(ax, a, r, max_x):
     vals = [a * pow(r, i) for i in range(max_x)]
     ax.plot(range(max_x), vals, color='k')
@@ -76,4 +111,22 @@ if __name__ == '__main__':
     df, rmdf, cdf, cfs = modeling.get_redundant_data(cfs, idf)
 
     #plot_empirical_p_idea(df)
-    plot_log_log_zipf(df)
+    #plot_log_log_zipf(df)
+
+    freqs = idea_freqs(df)
+    dat = gen_dat(freqs)
+    param_walks = modeling.compile_and_fit(model_string, dat, 3000, 3)
+    post_alpha_param = mystats.mean_and_hpd(param_walks[0]['alpha'])
+
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111)
+    pareto_fn = lambda x: pareto.pdf(x, post_alpha_param[0])
+    zipf_fn = lambda x: zipf.pmf(x, 1.03)
+
+    #print(list([(z, zipf.pmf(1, z)) for z in np.arange(1.01, 2.00, 0.01)]))
+
+    plot_freqs_against_dist(ax, freqs, zipf_fn)
+
+    #print(post_alpha_param[0])
+    plt.show()
+
